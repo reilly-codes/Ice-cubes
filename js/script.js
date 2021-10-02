8b1268fe7b72343ec7881cf80925d993e73968e6
reilly-codes <jroduory@gmail.com>
2021-10-02 09:13:57 +0300

    First draft

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// solution toggler
+function solution_one(){
+    var element = document.getElementById("hidden-one");
+    element.classList.toggle("hidden");
+}
+
+function solution_two(){
+    var element = document.getElementById("hidden-two");
+    element.classList.toggle("hidden");
+}
+
+function solution_three(){
+    var element = document.getElementById("hidden-three");
+    element.classList.toggle("hidden");
+}
+
+function solution_four(){
+    var element = document.getElementById("hidden-four");
+    element.classList.toggle("hidden");
+}
+
+function solution_five(){
+    var element = document.getElementById("hidden-five");
+    element.classList.toggle("hidden");
+}
+
+function solution_six(){
+    var element = document.getElementById("hidden-six");
+    element.classList.toggle("hidden");
+}
+
+function scrollfunction(id_name){
+    let about = document.getElementById(id_name); 
+    about.scrollIntoView()
+}
